db130da156cc3b7eb1aeceeaeac23f0a2a19df26
David Van Fleet <dvfleet413@gmail.com>
2020-01-31 02:31:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
   
   def execute_transaction
-    if self.status == "pending" && sender.balance > amount
+    if self.status == "pending" && sender.balance > amount && self.valid?
       sender.balance -= amount
       receiver.balance += amount
       self.status = "complete"
