4b5a5eafd8f2c033914823b2898a320526829bf6
David Van Fleet <dvfleet413@gmail.com>
2020-01-31 02:24:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,13 +1,17 @@
 class Transfer
-  attr_accessor :sender, :receiver, :amount
+  attr_accessor :sender, :receiver, :amount, :status
   
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @amount = amount
+    @status = "pending"
   end
   
   def valid?
     sender.valid? && receiver.valid?
   end
+  
+  def execute_transaction
+    @sender.
 end
