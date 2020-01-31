fdae932283bb4151456e8fd4ef5703b9cbf5408c
David Van Fleet <dvfleet413@gmail.com>
2020-01-31 02:18:32 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -9,7 +9,7 @@ class BankAccount
   end
   
   def deposit(amount)
-    balance += amount
+    @balance += amount
   end
   
   def display_balance
@@ -18,5 +18,9 @@ class BankAccount
   
   def valid?
     status == "open" && balance > 0
+  end
+  
+  def close_account
+    status = "closed"
   end
 end
